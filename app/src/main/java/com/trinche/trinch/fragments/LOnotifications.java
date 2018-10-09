c43b9037b6f465fd689b26c5fffb87a5c2a433ee
Norma <luciariquelme51@gmail.com>
2018-10-09 13:56:16 -0500

    notifications

--- app/src/main/java/com/trinche/trinch/fragments/LOnotifications.java
+++ app/src/main/java/com/trinche/trinch/fragments/LOnotifications.java
@@ -3,18 +3,28 @@ package com.trinche.trinch.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.trinche.trinch.R;
+import com.trinche.trinch.adapters.RVNotifications;
 
 public class LOnotifications extends Fragment {
 
+    RecyclerView RVNotificacion;
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.layout_notifications, container, false);
+        View v = inflater.inflate(R.layout.frag_rv_notif, container, false);
+
+        RVNotificacion = (RecyclerView) v.findViewById(R.id.RVNotificacion);
+        RVNotificacion.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        RVNotifications adapterNotifications = new RVNotifications();
+        RVNotificacion.setAdapter(adapterNotifications);
         return v;
     }
 }
